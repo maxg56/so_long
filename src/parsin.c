622904de9a7d8d9fbad3dc7a75ad989e0efac9e3
Maxg56 <Mgendrot56@gmail.com>
2024-12-10 11:01:49 +0100

    fin

--- src/parsin.c
+++ src/parsin.c
@@ -6,7 +6,7 @@
 /*   By: mgendrot <mgendrot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 02:36:24 by mgendrot          #+#    #+#             */
-/*   Updated: 2024/12/10 10:51:31 by mgendrot         ###   ########.fr       */
+/*   Updated: 2024/12/10 11:00:20 by mgendrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,41 @@ static int	open_map(char *path)
 	return (fd);
 }
 
+static size_t	get_line_length(char *line)
+{
+	size_t	len;
+
+	len = ft_strlen(line);
+	if (line[len - 1] == '\n')
+		len--;
+	return (len);
+}
+
 static t_bool	check_map_len(char *path, t_map *map, char *maps)
 {
 	char	*line;
+	char	*temp;
 	int		fd_map;
+	size_t	line_len;
 
 	fd_map = open_map(path);
 	line = get_next_line(fd_map);
-	map->height++;
+	if (!line)
+		return (FALSE);
+	map->height = 0;
 	while (line)
 	{
+		line_len = get_line_length(line);
 		if (map->width == 0)
-			map->width = ft_strlen(line);
-		else if (map->width != (int)ft_strlen(line))
-			return (FALSE);
+			map->width = get_line_length(line);
+		else if (map->width != (int)line_len)
+			return (free(line), free(maps), close(fd_map), FALSE);
+		temp = maps;
 		maps = ft_strjoin(maps, line);
-		map->height++;
-		free(line);
+		(free(temp), free(line));
 		line = get_next_line(fd_map);
+		map->height++;
 	}
-	free(line);
 	close(fd_map);
 	return (TRUE);
 }
@@ -55,17 +70,22 @@ t_bool	parse_input(int ac, char **av, t_game *game)
 	char		*path;
 	char		*maps;
 
-	ft_printf("1");
 	maps = ft_strdup("");
+	if (!maps)
+		return (FALSE);
 	if (ac == 1)
-		path ="maps/p1.bar";
+		path = "maps/p1.bar";
 	else
 		path = av[1];
-	check_map_len(path, game->maps, maps);
-	ft_printf("2");
-	ft_printf("%s\n", maps);
+	if (!check_map_len(path, game->maps, maps))
+	{
+		free(maps);
+		return (FALSE);
+	}
 	game->maps->map = (const char **)ft_split(maps, '\n');
-	ft_printf("%d\n", game->maps->height);
+	free(maps);
+	if (!game->maps->map)
+		return (FALSE);
 	if (check_map_vide(game))
 		return (FALSE);
 	return (TRUE);
